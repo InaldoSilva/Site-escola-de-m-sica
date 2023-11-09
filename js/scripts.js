cf6521311eee408503a5f8a3b6b51c9a93c2663b
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-11-08 22:38:35 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$("#myTab a").on("click", function (e) {
+  e.preventDefault();
+  $(this).tab("show");
+});
+
+$('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+  e.target; // newly activated tab
+  e.relatedTarget; // previous active tab
+});
+
+$("#someTab").tab("show");
